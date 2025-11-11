9ba9db97848802e166f1c563d3b32eda269c352e
lilraffyel <148103435+lilraffyel@users.noreply.github.com>
2025-11-11 14:44:48 +0800

    Template for NSG and Resource Group made

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,9 +1,9 @@
-variable resource_group_name {
+variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
 }
 
-variable region {
+variable "location" {
   description = "The Azure region where the resource group will be created"
   type        = string
 }
